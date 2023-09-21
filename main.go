c7ab93d9043f7bf791a2180cb1a9267751056076
Gabriel Fosse <kraken+majesticio@users.noreply.github.com>
2023-09-21 17:56:08 -0600

    beefy

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"html/template"
 	"log"
@@ -14,6 +15,11 @@ var (
 	taskLock sync.Mutex
 )
 
+// Embed the index.html file
+//
+//go:embed index.html
+var content embed.FS
+
 type Task struct {
 	ID   int
 	Name string
@@ -23,11 +29,17 @@ func main() {
 	http.HandleFunc("/", displayTasks)
 	http.HandleFunc("/add-task/", addTask)
 	http.HandleFunc("/remove-task/", removeTask)
+	http.HandleFunc("/clear-tasks/", clearTasks)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
 func displayTasks(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("index.html"))
+	data, err := content.ReadFile("index.html")
+	if err != nil {
+		http.Error(w, "Failed to load template", http.StatusInternalServerError)
+		return
+	}
+	tmpl := template.Must(template.New("index.html").Parse(string(data)))
 	tmpl.Execute(w, map[string]interface{}{"Tasks": tasks})
 }
 
@@ -38,8 +50,12 @@ func addTask(w http.ResponseWriter, r *http.Request) {
 	name := r.PostFormValue("name")
 	taskID++
 	tasks = append(tasks, Task{ID: taskID, Name: name})
-
-	tmpl := template.Must(template.ParseFiles("index.html"))
+	data, err := content.ReadFile("index.html")
+	if err != nil {
+		http.Error(w, "Failed to load template", http.StatusInternalServerError)
+		return
+	}
+	tmpl := template.Must(template.New("index.html").Parse(string(data)))
 	tmpl.ExecuteTemplate(w, "todo-list-element", Task{ID: taskID, Name: name})
 }
 
@@ -48,7 +64,6 @@ func removeTask(w http.ResponseWriter, r *http.Request) {
 	defer taskLock.Unlock()
 
 	id := r.PostFormValue("id")
-	// fmt.Println("Received ID:", id)
 
 	for index, task := range tasks {
 		if fmt.Sprintf("%d", task.ID) == id {
@@ -57,7 +72,14 @@ func removeTask(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	// fmt.Println("Current tasks after removal:", tasks)
+	w.WriteHeader(http.StatusOK)
+}
+
+func clearTasks(w http.ResponseWriter, r *http.Request) {
+	taskLock.Lock()
+	defer taskLock.Unlock()
+
+	tasks = []Task{} // Clear the tasks slice
 
 	w.WriteHeader(http.StatusOK)
 }
